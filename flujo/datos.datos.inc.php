26967c21930c1a10633d9690bf31c9a139ddcbe7
Carlos Daniel Ramirez López <cramirezl@fcpn.edu.bo>
2022-12-12 02:24:25 -0400

    Delete flujo directory

--- flujo/datos.datos.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$sql="SELECT * FROM academico.alumno ";
-$sql.="WHERE id=100";
-$resultadofi=mysqli_query($con, $sql);
-$filafi=mysqli_fetch_array($resultadofi);
-$id=$filafi["id"];
-$nombrecompleto=$filafi["nombrecompleto"];
-$promedio=$filafi["promedio"];
-?>
